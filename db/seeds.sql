0b675ee3ea809497de72eff2759865376b218e7c
skinsler <simonkinsler@gmail.com>
2019-06-04 20:42:29 -0500

    db schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and Swiss', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', TRUE);
