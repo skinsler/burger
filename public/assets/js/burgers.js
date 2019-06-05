31c6b246be3db31a7668925dcc83f6e831255a2f
skinsler <simonkinsler@gmail.com>
2019-06-05 14:56:58 -0500

    added a frontend js

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,29 @@
+$(function() {
+    alert("Hi");
+    $(".devour-burger").on("click", function(event) {
+      event.preventDefault();
+      
+    });
+  
+    // $(".create-form").on("submit", function(event) {
+    //   // Make sure to preventDefault on a submit event.
+    //   event.preventDefault();
+  
+    //   var newCat = {
+    //     name: $("#ca").val().trim(),
+    //     sleepy: $("[name=sleepy]:checked").val().trim()
+    //   };
+  
+    //   // Send the POST request.
+    //   $.ajax("/api/cats", {
+    //     type: "POST",
+    //     data: newCat
+    //   }).then(
+    //     function() {
+    //       console.log("created new cat");
+    //       // Reload the page to get the updated list
+    //       location.reload();
+    //     }
+    //   );
+    // });
+  });
