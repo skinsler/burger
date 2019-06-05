b38a6b83282e9c04930053852d2a9c31089dde19
skinsler <simonkinsler@gmail.com>
2019-06-04 22:37:05 -0500

    added controllers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+const burger = require("../models/burger");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.create(req.body.name, function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/", function(req, res) {
+    burger.update(req.body.id, function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      });
+});
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+const orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+      orm.selectAll(function(res) {
+        cb(res);
+      });
+    },
+
+    create: function(burger_name, cb) {
+      orm.insertOne(burger_name, function(res) {
+        cb(res);
+      });
+    },
+    update: function(id, cb) {
+      orm.updateOne(id, function(res) {
+        cb(res);
+      });
+    }
+  };
+
+  module.exports = burger;
